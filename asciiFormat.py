8572784638e5b6c08124c6934f0f1f1c175a32a0
Ali <parsa201909@gmail.com>
2023-08-17 15:58:21 +0330

    version 3

--- asciiFormat.py
+++ asciiFormat.py
@@ -1,9 +1,15 @@
-import pyfiglet
-from termcolor2 import colored
 # Import the os module to create and write files
 import os
 # Import the PIL module to convert text to image
 from PIL import Image, ImageDraw, ImageFont
+# Import pyfiglet and termcolor2 modules to create ASCII art
+import pyfiglet
+from termcolor2 import colored
+import termcolor2
+# Import the time module to create delays between each frame of animation
+import time
+# Import the curses module to control the terminal output
+import curses
 
 try:
     text=input('Enter your text: ')
@@ -39,8 +45,9 @@ try:
             image = Image.new("RGB", (800, 600), (255, 255, 255))
             # Create a draw object to draw on the image
             draw = ImageDraw.Draw(image)
-            # Create a font object with the chosen font name and size
-            font = ImageFont.truetype(font_name + ".flf", 32)
+            # Create a font object with a different font name and size
+            # You can change this to any TrueType font file you have or download one from [here]
+            font = ImageFont.truetype("arial.ttf", 32)
             # Draw the ASCII art on the image with black color
             draw.text((10, 10), ascii_format, (0, 0, 0), font=font)
             # Save the image as png file
@@ -49,6 +56,64 @@ try:
         else:
             print("Invalid file format. Please try again.")
     else:
+        print("OK")
+    # Ask the user if they want to create animated ASCII art
+    animate = input("Do you want to create animated ASCII art? (y/n): ")
+    # If yes, ask the user for the number of frames and the speed of animation
+    if animate.lower() == "y":
+        frames = int(input("Enter the number of frames: "))
+        speed = float(input("Enter the speed of animation (in seconds): "))
+        # Initialize the curses module and get a window object
+        stdscr = curses.initscr()
+        # Turn on color support
+        curses.start_color()
+        # Define some color pairs using RGB values
+        # You can change these values or add more pairs as you like
+        curses.init_color(1, 1000, 0, 0) # Red
+        curses.init_color(2, 0, 1000, 0) # Green
+        curses.init_color(3, 0, 0, 1000) # Blue
+        curses.init_color(4, 1000, 1000, 0) # Yellow
+        curses.init_color(5, 1000, 0, 1000) # Magenta
+        curses.init_color(6, 0, 1000, 1000) # Cyan
+        curses.init_color(7, 1000, 1000, 1000) # White
+        
+        # Associate each color with a number from 1 to 7
+        curses.init_pair(1, 1, curses.COLOR_BLACK) # Red on black background
+        curses.init_pair(2, 2, curses.COLOR_BLACK) # Green on black background
+        curses.init_pair(3, 3, curses.COLOR_BLACK) # Blue on black background
+        curses.init_pair(4, 4, curses.COLOR_BLACK) # Yellow on black background
+        curses.init_pair(5, 5, curses.COLOR_BLACK) # Magenta on black background
+        curses.init_pair(6, 6, curses.COLOR_BLACK) # Cyan on black background
+        curses.init_pair(7, 7, curses.COLOR_BLACK) # White on black background
+        
+        # Turn off echoing of keys and enable keypad mode
+        curses.noecho()
+        curses.cbreak()
+        stdscr.keypad(True)
+        # Clear the screen and hide the cursor
+        curses.curs_set(0)
+        stdscr.clear()
+        # Loop through each frame of animation
+        for i in range(frames):
+            # Create a new ASCII art with the chosen font
+            ascii_format=pyfiglet.figlet_format(text, font=font_name)
+            # Get the attribute value for the color pair corresponding to the frame number
+            attr = curses.color_pair((i % 7) + 1)
+            # Add the ASCII art to the window object at position (0, 0) with the color attribute
+            stdscr.addstr(0, 0, ascii_format, attr)
+            # Refresh the window object to show the changes
+            stdscr.refresh()
+            # Wait for a specified amount of time before moving to the next frame
+            time.sleep(speed)
+        # Restore the terminal settings and close the window object
+        curses.nocbreak()
+        stdscr.keypad(False)
+        curses.echo()
+        curses.endwin()
+        print("Your animated ASCII art is done.")
+    else:
         print("OK. Have a nice day.")
 except:
     print("sorry! Something went wrong! Please try again")
+
+
